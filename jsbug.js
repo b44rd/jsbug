2707ad4617e24bc10440e983133c5f9cdd3f7828
Bård Øien <bard.oien@colorline.no>
2017-06-08 09:46:18 +0200

    Add support for passing color as option

--- jsbug.js
+++ jsbug.js
@@ -1,27 +1,25 @@
-/*
-This is free and unencumbered software released into the public domain.
+// This is free and unencumbered software released into the public domain.
 
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
 
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 (function(name, definition) {
   if (typeof module != 'undefined') module.exports = definition();
   else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
@@ -29,48 +27,43 @@ OTHER DEALINGS IN THE SOFTWARE.
 }('mod', function() {
   "use strict";
 
-  var i, color, fontsize, background, prepend, jsbug = localStorage.getItem("jsbug") !== null;
+  var fontsize = 12,
+      style = "color: #fff;font-size:" + fontsize + "pt;font-weight:normal;padding:2px 10px;border-radius:10px;",
+      prepend = "♢ ",
+      background = "#0088CF";
 
   // Turn on
   if (window.location.href.indexOf("jsbug=true") > -1) {
     localStorage.setItem("jsbug", true);
-    jsbug = true;
   }
 
   // Turn off
   if (window.location.href.indexOf("jsbug=false") > -1) {
     localStorage.setItem("jsbug", false);
-    jsbug = false;
   }
 
-  if (jsbug) {
-    window.console.log("%c! Debug enabled. Set jsbug=false to turn off", "color:#fff; background:#002C6D; font-size:12pt; font-weight: normal;  padding: 2px 10px; border-radius: 10px;");
+  if (localStorage.getItem("jsbug") !== null) {
+    window.console.log("%c! Debug enabled. Set jsbug=false to turn off", style + "background:#002C6D");
   }
 
   return function(str, options) {
-    if (jsbug) {
+    if (localStorage.getItem("jsbug") !== null) {
       options = options || {};
-      background = str.indexOf("|") === 0 ? "#FCB813" : "#0088CF";
-      background = options.timer ? "#fff": background;
-      color = options.timer ? "#ccc" : "#fff";
-      fontsize = options.timer ? 8 : 12;
-      prepend = "";
+      background = str.indexOf("|") === 0 ? "#FCB813" : options.color || background
 
       if (typeof options.success !== "undefined") {
         background = options.success ? "#00A551" : "#EC1C24";
         prepend = options.success ? "* " : "@ Failure! ";
       }
 
-      prepend = (prepend === "" && background === "#0088CF") ? "♢ " : prepend
-
       if (typeof options.group !== "undefined") {
-        window.console.groupCollapsed("%c" + prepend + str, "color:" + color + "; background:" + background + "; font-size:" + fontsize + "pt; font-weight: normal; padding: 2px 10px; border-radius: 10px;");
-        for (i = 0; i < options.group.length; i++){
+        window.console.groupCollapsed("%c" + prepend + str, style + "background:" + background);
+        for (var i = 0; i < options.group.length; i++){
           window.console.log(options.group[i]);
         }
         window.console.groupEnd();
       } else {
-        window.console.log("%c" + prepend + str, "color:" + color + "; background:" + background + "; font-size:" + fontsize + "pt; font-weight: normal;  padding: 2px 10px; border-radius: 10px;");
+        window.console.log("%c" + prepend + str, style + "background:" + background);
       }
     }
   }
